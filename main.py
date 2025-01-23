447f641ac71e828a55a13f1d291e4aafc44fdb22
bossy228 <arthom.koval@gmail.com>
2025-01-23 23:06:09 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from faker import Faker
+from time import time
+
+
+api = FastAPI()
+faker = Faker()
+
+
+@api.get('/')
+async def root_read(request: Request):
+    return RedirectResponse(f'{request.url}docs')
+
+
+
+@api.get('/name')
+async def faker_name():
+    return {'name': faker.name() , 'Time': time() }
+
+
+
+@api.get('/email')
+async def faker_email():
+    return {'email': faker.email() , 'Time': time() }
+
+
+
+@api.get('/password')
+async def faker_password():
+    return {'name': faker.password() , 'Time': time() }
+
+
+
